1a6539b5b6263fa18b553851a370a00c2e82b06e
kloot <claudesoft@gmail.com>
2024-03-31 23:58:29 +0200

    improve template & add fdv

--- generate.rb
+++ generate.rb
@@ -1,31 +1,51 @@
 require 'httparty'
 require 'haml'
 
-# Methode zum Abrufen der Preise
-def fetch_price(chain_id, pair_address)
+# Methode zum Abrufen der Preise und FDV
+def fetch_token_data(chain_id, pair_address)
   url = "https://api.dexscreener.com/latest/dex/pairs/#{chain_id}/#{pair_address}"
   response = HTTParty.get(url)
   if response.success?
     data = response.parsed_response["pairs"].first
     price_usd = data["priceUsd"]
-    price_usd
+    fdv = data["fdv"].to_f # Konvertierung in Float für mathematische Operationen
+    { price_usd: price_usd, fdv: fdv }
   else
-    "N/A"
+    { price_usd: "N/A", fdv: 0.0 }
   end
 end
 
-# Preise abrufen
-eth_price = fetch_price('ethereum', '0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640')
-cake_price = fetch_price('bsc', '0x7f51c8aaa6b0599abd16674e2b17fec7a9f674a1')
-avax_price = fetch_price('avalanche', '0xf4003f4efbe8691b60249e6afbd307abe7758adb')
+# FDV-Prozentberechnungsfunktion
+def calculate_fdv_percentage(data_array)
+  fdv_values = data_array.map { |data| data[:fdv] }
+  min_fdv = fdv_values.min
+  max_fdv = fdv_values.max
+  range = max_fdv - min_fdv
+  data_array.each do |data|
+    # Vermeidung einer Division durch Null, falls alle FDV-Werte gleich sind
+    if range > 0
+      data[:fdv_percentage] = ((data[:fdv] - min_fdv) / range * 100).round(2)
+    else
+      data[:fdv_percentage] = 0.0
+    end
+  end
+end
 
-# HAML-Template rendern
-#template = File.read('template.haml')
-#puts template
-engine = Haml::Template.new('template.haml')
-output = engine.render(Object.new, eth_price: eth_price, cake_price: cake_price, avax_price: avax_price)
+# Token-Daten abrufen
+eth_data = fetch_token_data('ethereum', '0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640')
+cake_data = fetch_token_data('bsc', '0x7f51c8aaa6b0599abd16674e2b17fec7a9f674a1')
+avax_data = fetch_token_data('avalanche', '0xf4003f4efbe8691b60249e6afbd307abe7758adb')
 
-# Ausgabe in index.html schreiben
+token_data_array = [eth_data, cake_data, avax_data]
+
+# FDV-Prozentwerte berechnen
+calculate_fdv_percentage(token_data_array)
+
+# HAML-Template rendern und Ausgabe in index.html schreiben
+engine = Haml::Template.new("template.haml")
+
+output = engine.render(Object.new, eth_data: eth_data, cake_data: cake_data, avax_data: avax_data)
+
 File.open("index.html", "w") do |file|
   file.puts output
 end
